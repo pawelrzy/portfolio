601253f18146820da0daa8d0b5e01555aa110243
Michael Vytlingam <michael.vytlingam@gmail.com>
2019-10-26 15:08:13 -0400

    Skills section complete

--- src/App/Knowledge/Knowledge.tsx
+++ src/App/Knowledge/Knowledge.tsx
@@ -1,39 +1,36 @@
 import React, { FC } from 'react';
 
+import './Knowledge.css';
+
 const Knowledge: FC = () => {
     const languages = ['Typescript/Javascript', 'Python', 'Go', 'SQL', 'Bash', 'MATLAB', 'Swift', 'C#', 'C++'];
     return (
-        <section className="container">
+        <section className="container knowledge">
             <h1 className="title">Knowledge</h1>
-            <div className="level">
-                <div className="level-left">
-                    <div className="card">
-                        <div className="card-content">
-                            <h3 className="title is-4">Languages</h3>
-                            <div className="tags are-medium">
-                                { languages.map(language => (
-                                    <span className="tag">
-                                        {language}
-                                    </span>
-                                ))}
-                            </div>
+            <div className="tile is-ancestor">
+                <div className="tile is-parent">
+                    <article className="tile is-child box">
+                        <h3 className="title is-6">Languages</h3>
+                        <div className="tags are-medium">
+                            { languages.map(language => (
+                                <span className="tag">
+                                    {language}
+                                </span>
+                            ))}
                         </div>
-                    </div>
-                    
+                    </article>
                 </div>
-                <div className="level-right">
-                    <div className="card">
-                        <div className="card-content">
-                            <h3 className="title is-4">Technologies</h3>
-                            <div className="tags are-medium">
-                                { languages.map(language => (
-                                    <span className="tag">
-                                        {language}
-                                    </span>
-                                ))}
-                            </div>
+                <div className="tile is-parent">
+                    <article className="tile is-child box">
+                        <h3 className="title is-6">Technologies</h3>
+                        <div className="tags are-medium">
+                            { languages.map(language => (
+                                <span className="tag">
+                                    {language}
+                                </span>
+                            ))}
                         </div>
-                    </div>
+                    </article>
                 </div>
             </div>
         </section>
