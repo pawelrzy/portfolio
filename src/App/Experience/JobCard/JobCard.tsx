5ca09e4f015bcb7f1190341aae4f3dc046d42fb4
Michael Vytlingam <michael.vytlingam@gmail.com>
2019-12-31 17:48:42 -0500

    Removes dangerouslySetInnerHTML

--- src/App/Experience/JobCard/JobCard.tsx
+++ src/App/Experience/JobCard/JobCard.tsx
@@ -1,37 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useEffect } from 'react';
 import './JobCard.scss';
 
 interface JobCardProps {
-    job: any //TODO
+    job: any; //TODO
 }
 
 const JobCard: FC<JobCardProps> = (props: JobCardProps) => {
+    let descriptionRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        descriptionRef.current!.innerHTML = job.body;
+    });
+
     const { job } = props;
     return (
         <article className="message job-card">
-            {job.hasOwnProperty('coverPhoto') && 
-                <div 
+            {job.hasOwnProperty('coverPhoto') && (
+                <div
                     className="message-header cover-photo"
-                    style={{backgroundImage: `url('${require(`../../../assets/${job.coverPhoto}`)}')`}} />
-            }
+                    style={{
+                        backgroundImage: `url('${require(`../../../assets/${job.coverPhoto}`)}')`
+                    }}
+                />
+            )}
             <div className="message-body">
                 <div className="title-row">
-                    <img 
-                        src={require(`../../../assets/${job.logo}`)} 
+                    <img
+                        src={require(`../../../assets/${job.logo}`)}
                         alt={`${job.company} logo`}
                         className="company-logo"
                     />
                     <div className="company-name">
                         <div>
-                            <h1 className="is-size-6"><b>{job.company} /</b>&nbsp;{job.position}</h1>
+                            <h1 className="is-size-6">
+                                <b>{job.company} /</b>&nbsp;{job.position}
+                            </h1>
                         </div>
-                        <p className="has-text-grey is-size-7">{job.location} | {job.date}</p>
+                        <p className="has-text-grey is-size-7">
+                            {job.location} | {job.date}
+                        </p>
                     </div>
                 </div>
-                <p dangerouslySetInnerHTML={{__html: job.body}}></p>
+                <div ref={descriptionRef} />
             </div>
         </article>
     );
-}
+};
 
 export default JobCard;
