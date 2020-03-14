fab20b075261b08ef54ba1d445bbca805d49e4f2
Michael Vytlingam <michael.vytlingam@gmail.com>
2020-03-14 19:56:08 -0400

    Reduces Knowledge component

--- src/App/Knowledge/Knowledge.tsx
+++ src/App/Knowledge/Knowledge.tsx
@@ -6,76 +6,79 @@ import Tags from './Tags/Tags';
 import './Knowledge.scss';
 
 const Knowledge: FC = () => {
-    const languages = [
-        'TypeScript',
-        'JavaScript',
-        'Python',
-        'C#',
-        'Java',
-        'Go',
-        'SQL',
-        'MATLAB',
-        'HTML5',
-        'Sass'
+    const skills = [
+        {
+            title: 'Languages',
+            tags: [
+                'TypeScript',
+                'JavaScript',
+                'Python',
+                'C#',
+                'Java',
+                'Go',
+                'SQL',
+                'MATLAB',
+                'HTML5',
+                'Sass'
+            ]
+        },
+        {
+            title: 'Front End',
+            tags: [
+                'React',
+                'Redux',
+                'Vue',
+                'Electron',
+                'JEST',
+                'Enzyme',
+                'Webpack',
+                'Babel',
+                'Bulma',
+                'Lottie',
+                'Figma',
+                'Gatsby',
+                'Apollo GraphQL',
+                'Semantic UI',
+                'Adobe Illustrator',
+                'Adobe Photoshop',
+                'Adobe After Effects'
+            ]
+        },
+        {
+            title: 'Back End + Infrastructure',
+            tags: [
+                'Express',
+                '.NET Core',
+                'AWS S3',
+                'Firebase',
+                'Docker',
+                'Azure DevOps (Pipelines)',
+                'CircleCI',
+                'AWS EC2'
+            ]
+        },
+        {
+            title: 'Data',
+            tags: [
+                'PostgreSQL',
+                'MongoDB',
+                'TensorFlow',
+                'YOLO',
+                'Elasticsearch',
+                'Microsoft Cognitive Services (LUIS)'
+            ]
+        }
     ];
-    const frontend = [
-        'React',
-        'Redux',
-        'Vue',
-        'Electron',
-        'JEST',
-        'Enzyme',
-        'Webpack',
-        'Babel',
-        'Bulma',
-        'Lottie',
-        'Figma',
-        'Gatsby',
-        'Apollo GraphQL',
-        'Semantic UI',
-        'Adobe Illustrator',
-        'Adobe Photoshop',
-        'Adobe After Effects'
-    ];
-    const backendAndInfrastructure = [
-        'Express',
-        '.NET Core',
-        'AWS S3',
-        'Firebase',
-        'Docker',
-        'Azure DevOps (Pipelines)',
-        'CircleCI',
-        'AWS EC2'
-    ];
-    const data = [
-        'PostgreSQL',
-        'MongoDB',
-        'TensorFlow',
-        'YOLO',
-        'Elasticsearch',
-        'Microsoft Cognitive Services (LUIS)'
-    ];
+
     return (
         <div className="knowledge">
             <article>
-                <div className="category">
-                    <h1 className="title is-size-4">Languages</h1>
-                    <Tags tags={languages} />
-                </div>
-                <div className="category">
-                    <h1 className="title is-size-4">Front End</h1>
-                    <Tags tags={frontend} />
-                </div>
-                <div className="category">
-                    <h1 className="title is-size-4">
-                        Back End + Infrastructure
-                    </h1>
-                    <Tags tags={backendAndInfrastructure} />
-                </div>
-                <div className="category">
-                    <h1 className="title is-size-4">Data</h1>
-                    <Tags tags={data} />
-                </div>
+                {skills.map(skill => (
+                    <div className="category">
+                        <h1 className="title is-size-4">{skill.title}</h1>
+                        <Tags tags={skill.tags} />
+                    </div>
+                ))}
             </article>
         </div>
     );
