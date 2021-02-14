4a708c91cc310ee444246aea002b2fec1e81c08f
Michael Vytlingam <michael.vytlingam@gmail.com>
2021-02-13 22:46:16 -0500

    adds hover to knowledge

--- src/App/Knowledge/Tags/Tags.tsx
+++ src/App/Knowledge/Tags/Tags.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import './Tags.scss';
+
 interface TagsProps {
     tags: string[];
 }
@@ -7,7 +9,7 @@ interface TagsProps {
 const Tags: FC<TagsProps> = (props: TagsProps) => (
     <div className="tags">
         {props.tags.map((tag, index) => (
-            <span key={index} className="tag is-info is-rounded is-light">
+            <span key={index} className="tag is-info is-rounded is-light skill">
                 {tag}
             </span>
         ))}
