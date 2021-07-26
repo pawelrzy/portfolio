fce323959cd0a41b8e32bd67014c4358f6ca1487
Michael Vytlingam <michael.vytlingam@gmail.com>
2021-07-25 18:33:43 -0700

    add styles to experiences

--- src/App/ContactMe/ContactMe.tsx
+++ src/App/ContactMe/ContactMe.tsx
@@ -1,66 +1,68 @@
 import React from 'react';
 
 import withCenterFormatting from '../../HOCs/withCenterFormatting/withCenterFormatting';
+import { ThemeContext } from '../App';
 import './ContactMe.scss';
 
-const ContactMe = () => (
-    <div className="contact-me">
-        <article className="message">
-            <div className="message-body">
-                <form
-                    name="input"
-                    className="field"
-                    method="POST"
-                    action={'https://formspree.io/meqjzody'}
-                >
-                    <div className="field">
-                        <label className="label">Name</label>
-                        <div className="control">
-                            <input
-                                className="input"
-                                type="text"
-                                name="Name"
-                                placeholder="Your name..."
-                            />
-                        </div>
-                    </div>
+const ContactMe = () => {
+    const isDarkTheme = React.useContext(ThemeContext);
 
-                    <div className="field">
-                        <label className="label">Email</label>
-                        <div className="control">
-                            <input
-                                className="input"
-                                type="email"
-                                name="_replyto"
-                                placeholder="Your email..."
-                            />
+    return (
+        <div className="contact-me">
+            <article className={`message ${isDarkTheme && 'dark-layer-1'}`}>
+                <div className="message-body">
+                    <form
+                        name="input"
+                        className="field"
+                        method="POST"
+                        action={'https://formspree.io/meqjzody'}
+                    >
+                        <div className="field">
+                            <label className="label">Name</label>
+                            <div className="control">
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="Name"
+                                    placeholder="Your name..." />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="field">
-                        <label className="label">Message</label>
-                        <div className="control">
-                            <textarea
-                                name="message"
-                                className="textarea"
-                                placeholder="Type your message"
-                            ></textarea>
+                        <div className="field">
+                            <label className="label">Email</label>
+                            <div className="control">
+                                <input
+                                    className="input"
+                                    type="email"
+                                    name="_replyto"
+                                    placeholder="Your email..." />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="field is-grouped send-button">
-                        <div className="control">
-                            <input
-                                className="button"
-                                type="submit"
-                                value="Send 📬"
-                            />
+                        <div className="field">
+                            <label className="label">Message</label>
+                            <div className="control">
+                                <textarea
+                                    name="message"
+                                    className="textarea"
+                                    placeholder="Type your message"
+                                ></textarea>
+                            </div>
                         </div>
-                    </div>
-                </form>
-            </div>
-        </article>
-    </div>
-);
+
+                        <div className="field is-grouped send-button">
+                            <div className="control">
+                                <input
+                                    className="button"
+                                    type="submit"
+                                    value="Send 📬" />
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </article>
+        </div>
+    );
+};
 
 export default withCenterFormatting('Contact Me', ContactMe);
