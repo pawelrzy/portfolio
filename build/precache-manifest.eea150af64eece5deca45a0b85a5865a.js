29e161c02d7858d05cbf969455c4c86f438242bc
Michael Vytlingam <michael.vytlingam@gmail.com>
2024-01-24 19:04:56 -0800

    add static

---
+++ build/precache-manifest.eea150af64eece5deca45a0b85a5865a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8aac93b1bd75ebd2e0c801c419868b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a1367e0779efbae83aa",
+    "url": "/static/css/main.de2676e4.chunk.css"
+  },
+  {
+    "revision": "e0df6e1941d4b7ad7a72",
+    "url": "/static/js/2.07189825.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/static/js/2.07189825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1367e0779efbae83aa",
+    "url": "/static/js/main.e4dea17c.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  },
+  {
+    "revision": "9f300e916f3be36fb9ffd76029fdc766",
+    "url": "/static/media/aboutme-selfie.9f300e91.jpg"
+  },
+  {
+    "revision": "b38ad7c64405063dfa16ae78a4253298",
+    "url": "/static/media/bb.b38ad7c6.png"
+  },
+  {
+    "revision": "fca93361aa4728ac00d070c1c2b13575",
+    "url": "/static/media/contact-pattern.fca93361.png"
+  },
+  {
+    "revision": "86041f7d1cd63ba788fcd4e29a914990",
+    "url": "/static/media/cricket.86041f7d.gif"
+  },
+  {
+    "revision": "8c639675bc7300fdd370ac35e5db800a",
+    "url": "/static/media/engineering-cover.8c639675.jpg"
+  },
+  {
+    "revision": "bdbf70c038a8d9f25822e5c5733dbb7e",
+    "url": "/static/media/engineering.bdbf70c0.jpg"
+  },
+  {
+    "revision": "8dd1ec503aa221d53aada756553e9e27",
+    "url": "/static/media/gd-cover.8dd1ec50.jpg"
+  },
+  {
+    "revision": "ce1b941c992a76474f4094d189b77137",
+    "url": "/static/media/gd.ce1b941c.jpg"
+  },
+  {
+    "revision": "5794420a7eaa35fb49d13e8b56c240a2",
+    "url": "/static/media/kendo.5794420a.jpg"
+  },
+  {
+    "revision": "3b3a10f8a26f13681d70b6fa8b773e1f",
+    "url": "/static/media/knowledge-pattern.3b3a10f8.png"
+  },
+  {
+    "revision": "2f67d006b09e74286700c5ec932f6f54",
+    "url": "/static/media/logo.2f67d006.jpg"
+  },
+  {
+    "revision": "02f4974b4ebfaed6eb7e13e435f4a428",
+    "url": "/static/media/resume.02f4974b.pdf"
+  },
+  {
+    "revision": "74a35a9cdd5f60e7aa7455a8835e3611",
+    "url": "/static/media/rollout.74a35a9c.png"
+  },
+  {
+    "revision": "971593b736a7986b4802407a23147805",
+    "url": "/static/media/selfie.971593b7.jpg"
+  },
+  {
+    "revision": "95e6b3d86c6f75375c9ed9792d1c9f79",
+    "url": "/static/media/signature.95e6b3d8.png"
+  },
+  {
+    "revision": "f8c063621331dd0558e0a6bcdf686c4f",
+    "url": "/static/media/snowflake.f8c06362.png"
+  },
+  {
+    "revision": "df3f7acaccbf2b9db7fe17d3f6310a26",
+    "url": "/static/media/snyder.df3f7aca.jpg"
+  },
+  {
+    "revision": "ab5ada0d13268d238aeea775a0fbfcf7",
+    "url": "/static/media/sudoku.ab5ada0d.gif"
+  },
+  {
+    "revision": "28f6983ced8651f58564f5f8397d6795",
+    "url": "/static/media/uw.28f6983c.jpg"
+  }
+]);
